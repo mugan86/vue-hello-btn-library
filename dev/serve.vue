321de487fb4b9b7f80edef6e2030c7359c16780f
Anartz Mugika Ledo <mugan86@gmail.com>
2020-07-29 20:24:25 +0200

    Finish library example

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { HelloBtn } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    HelloBtn,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <HelloBtn />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as HelloBtn } from './vue-hello-btn.vue';
